844fa6f4ea538e003b6dad75697f2b2fdc295959
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-04-07 19:26:10 +0300

    feat: update folders structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Header } from "./components";
 import "./index.css";
-import App from "./App";
+
+const App = () => {
+  return <Header />;
+};
 
 ReactDOM.render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#EB5757",
+        light: "#F2F2F2",
+        gray: "#BDBDBD",
+        main: "#333333",
+        textLighter: "#4F4F4F",
+        textLightest: "#828282",
+      },
+    },
   },
   plugins: [],
 };
