052b81a5a9094aabcfee32022189a34236afd3fa
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-04-07 19:25:17 +0300

    feat: add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./assets/logo.png";
+import { Search } from "../Search";
+
+export const Header = () => {
+  return (
+    <div className="md:flex md:justify-between md:align-middle md:mt-8 md:container md:mx-auto">
+      <div className="w-100">
+        <a href="#" className="inline-block py-5 px-3 hover:cursor-pointer">
+          <img src={logo} alt="Windbnb logo" />
+        </a>
+      </div>
+      <Search location="Helsinki, Finland" numOfGuests={4} />
+    </div>
+  );
+};
