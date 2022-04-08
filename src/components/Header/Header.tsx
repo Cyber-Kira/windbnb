51ef8e49baab061bf6421e0ef1950963803a6fae
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-04-08 18:32:17 +0300

    feat: add padding to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import { Search } from "../Search";
 
 export const Header = () => {
   return (
-    <div className="md:flex md:justify-between md:align-middle md:mt-8 md:container md:mx-auto">
+    <div className="md:flex md:justify-between md:align-middle md:mt-8 md:container md:mx-auto px-5">
       <div className="w-100">
         <a href="#" className="inline-block py-5 px-3 hover:cursor-pointer">
           <img src={logo} alt="Windbnb logo" />
